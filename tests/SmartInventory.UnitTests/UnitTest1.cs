907f9bba01c2415dc4eac88ad19bcb2682a3b8ab
GlennClaesPXL <148762236+GlennClaesPXL@users.noreply.github.com>
2025-10-24 16:44:29 +0200

    Initial commit

---
+++ tests/SmartInventory.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SmartInventory.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
